c96f28ea1a20a86b95c93661c8b9026dabe32dde
Babin Dahal <babin.dahal3@gmail.com>
2016-10-13 12:05:19 +1100

    Implement deleting tickets features

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -26,6 +26,27 @@ def show
 
 end
 
+def edit
+
+end
+
+def update
+  if @ticket.update(ticket_params)
+    flash[:notice] = "Ticket has been updated."
+    redirect_to [@project, @ticket]
+  else
+    flash.now[:alert] = "Ticket has not beeb updated."
+    render :edit
+  end
+end
+
+def destroy
+  @ticket.destroy
+  flash[:notice] = "Ticket has been deleted."
+
+  redirect_to @project
+end
+
 private
 
 def set_project
