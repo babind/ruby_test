1fbc9e2005a1d05eb606db8e449f08a9dc659167
Babin Dahal <babin.dahal3@gmail.com>
2016-10-13 12:32:20 +1100

    Link Tickets and users upon tickets creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :project
+  belongs_to :author, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013000913) do
+ActiveRecord::Schema.define(version: 20161013011718) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20161013000913) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "author_id"
+    t.index ["author_id"], name: "index_tickets_on_author_id"
     t.index ["project_id"], name: "index_tickets_on_project_id"
   end
 
